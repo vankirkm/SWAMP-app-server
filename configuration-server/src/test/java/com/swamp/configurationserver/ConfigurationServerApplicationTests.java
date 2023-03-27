42ef52882bd1ce9e68b8523f02d32e8e4b8ffd77
Michael Van Kirk <vankirk.6@wright.edu>
2023-03-27 17:34:58 -0400

    adds server code

---
+++ configuration-server/src/test/java/com/swamp/configurationserver/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swamp.configurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
